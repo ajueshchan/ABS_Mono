54d84dbd72895a13d0a8f6115e2864af9857e339
ajueshchan <nagarjunjeyachandran@gmail.com>
2025-12-07 22:20:08 +0530

    Create trainers table in appointment_db

--- sql/trainers.sql
+++ sql/trainers.sql
@@ -1,5 +1,5 @@
-CREATE TABLE appointment_db."providers" (
-    provider_id BIGINT GENERATED ALWAYS AS IDENTITY (
+CREATE TABLE appointment_db."trainers" (
+    trainer_id BIGINT GENERATED ALWAYS AS IDENTITY (
         START WITH 1
         INCREMENT BY 1
     ) PRIMARY KEY,
@@ -9,7 +9,6 @@ CREATE TABLE appointment_db."providers" (
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-   CONSTRAINT fk_provider_user FOREIGN KEY (user_id) REFERENCES appointment_db."users"(user_id) 
+   CONSTRAINT fk_trainer_user FOREIGN KEY (user_id) REFERENCES appointment_db."users"(user_id) 
    ON DELETE CASCADE ON UPDATE CASCADE
-   )
+)
