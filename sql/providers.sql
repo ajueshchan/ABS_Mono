f8ed762f6444a2126bfd835c8407fac992826e35
ajueshchan <nagarjunjeyachandran@gmail.com>
2025-12-07 21:21:27 +0530

    Create providers table in appointment_db

---
+++ sql/providers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE appointment_db."providers" (
+    provider_id BIGINT GENERATED ALWAYS AS IDENTITY (
+        START WITH 1
+        INCREMENT BY 1
+    ) PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+   CONSTRAINT fk_provider_user FOREIGN KEY (user_id) REFERENCES appointment_db."users"(user_id) 
+   ON DELETE CASCADE ON UPDATE CASCADE
+   )
