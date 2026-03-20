4168e33ac28004444ef7192f8590b9dcf3835783
Nagarjun <nagarjunjeyachandran@gmail.com>
2026-03-20 15:13:40 +0530

    mail_Template table is added

---
+++ sql/mail_template.sql
@@ -0,0 +1,11 @@
+CREATE TABLE mail_template (
+    id              BIGSERIAL       NOT NULL,
+    template_name   VARCHAR(100)    NOT NULL,
+    short_message   VARCHAR(255)    NOT NULL,
+    long_message    TEXT            NOT NULL,
+    created_ts      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_ts      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status          BOOLEAN         NOT NULL DEFAULT 'Active',
+
+    CONSTRAINT uq_mail_template_name    UNIQUE (template_name)
+);
