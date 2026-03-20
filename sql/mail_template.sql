94e67ba6dacde24ecb9d0f50f12c3cf7721af127
ajueshchan <nagarjunjeyachandran@gmail.com>
2026-03-20 18:18:11 +0530

    Add primary key constraint to mail_template table

--- sql/mail_template.sql
+++ sql/mail_template.sql
@@ -7,5 +7,6 @@ CREATE TABLE mail_template (
     updated_ts      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status          BOOLEAN         NOT NULL DEFAULT 'Active',
 
+    CONSTRAINT pk_mail_template         PRIMARY KEY (id),
     CONSTRAINT uq_mail_template_name    UNIQUE (template_name)
 );
